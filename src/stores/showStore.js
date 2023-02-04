cd70aa8a239a0459fb7fff3c174cdf2a4b322a54
Danny <barakadan421@gmail.com>
2023-02-05 00:16:01 +0300

    Prices chart

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -2,9 +2,21 @@ import axios from 'axios'
 import { create } from 'zustand'
 
 const showStore = create((set) => ({
+    graphData: [],
+
     fetchData: async (id) => {
-        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`)
-        console.log(res.data);
+        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`)
+
+        const graphData = res.data.prices.map((price) => {
+            const [timestamp, p] = price;
+            const date = new Date(timestamp).toLocaleDateString("en-us")
+            return {
+                Date: date,
+                Price: p,
+            }
+        })
+
+        set({ graphData })
     }
 
 }));
