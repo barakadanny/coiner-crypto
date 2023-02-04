18b70567a145d103b0f714b2d3244dbc1b50d711
Danny <barakadan421@gmail.com>
2023-02-04 21:18:21 +0300

    Search function in the store

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -3,6 +3,21 @@ import { create } from 'zustand'
 
 const homeStore = create((set) => ({
   coins: [],
+  query: '',
+
+  setQuery: (e) => {
+    set({query: e.target.value})
+    homeStore.getState().searchCoins()
+  },
+
+  searchCoins: async () => {
+    const { query } = homeStore.getState()
+    const res = await axios.get('https://api.coingecko.com/api/v3/search?query=${query}')
+    // const res = await axios.get('https://api.coingecko.com/api/v3/search?query=${query}&per_page=100&page=1&sparkline=false')
+    console.log(res)
+    // const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(query.toLowerCase()))
+    // set({coins: filteredCoins})
+  },
 
   fetchCoins: async () => {
     const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
