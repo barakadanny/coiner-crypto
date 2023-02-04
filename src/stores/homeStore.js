3dd2f1fcd5fa717f3003fbff69d19acac04a9c41
Danny <barakadan421@gmail.com>
2023-02-04 20:52:40 +0300

    Extract uselful data in object

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -2,9 +2,20 @@ import axios from 'axios'
 import { create } from 'zustand'
 
 const homeStore = create((set) => ({
+  coins: [],
+
   fetchCoins: async () => {
-    const res = axios.get('https://api.coingecko.com/api/v3/search/trending')
-    console.log(res)
+    const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
+
+    const coins = res.data.coins.map(coins => {
+        return {
+            name: coins.item.name,
+            image: coins.item.large,
+            id: coins.item.id,
+            princeBtc: coins.item.price_btc
+        }
+    })
+    console.log(coins)
   }
 }))
 
