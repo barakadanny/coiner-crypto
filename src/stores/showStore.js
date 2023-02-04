99b2e1bc0bf4b805b046106ce6844f054132a8ef
Danny <barakadan421@gmail.com>
2023-02-05 01:18:42 +0300

    Show data details

--- src/stores/showStore.js
+++ src/stores/showStore.js
@@ -5,9 +5,12 @@ const showStore = create((set) => ({
     graphData: [],
 
     fetchData: async (id) => {
-        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`)
+        const [graphRes, dataRes] = await Promise.all([
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`),
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`),
+        ])
 
-        const graphData = res.data.prices.map((price) => {
+        const graphData = graphRes.data.prices.map((price) => {
             const [timestamp, p] = price;
             const date = new Date(timestamp).toLocaleDateString("en-us")
             return {
@@ -16,7 +19,12 @@ const showStore = create((set) => ({
             }
         })
 
-        set({ graphData })
+        console.log(dataRes.data)
+        const { name, symbol, image} = dataRes.data;
+        const imageLarge = image.large
+        
+        set({ graphData, name, symbol, imageLarge });
+
     }
 
 }));
