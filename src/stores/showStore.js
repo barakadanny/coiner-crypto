b85e50b70f283ede418c86c72fe75f0d0e31cd17
Danny <barakadan421@gmail.com>
2023-02-04 23:30:35 +0300

    showStore api end point

---
+++ src/stores/showStore.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import { create } from 'zustand'
+
+const showStore = create((set) => ({
+    fetchData: async (id) => {
+        const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7`)
+        console.log(res.data);
+    }
+
+}));
+
+export default showStore
