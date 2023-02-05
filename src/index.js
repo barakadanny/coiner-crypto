ad037d07381cd476cd7f93e0f660ab0f1af38535
Danny <barakadan421@gmail.com>
2023-02-05 17:55:27 +0300

    Tailwind app.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Show from './pages/Show';
+import './style.scss'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
