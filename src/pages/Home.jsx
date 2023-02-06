09e1ad934822881df59e825ed4380f2e386039c9
Danny <barakadan421@gmail.com>
2023-02-06 15:43:41 +0300

    Add Banner and style|| home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { React, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import homeStore  from '../stores/homeStore'
+import Header from '../components/Header'
 
 function Home() {
     const store = homeStore()
@@ -9,7 +10,23 @@ function Home() {
     }, [])
   return (
     <div>
-        <input type="text" value={store.query} onChange={store.setQuery} />
+      <Header />
+      <div className='banner bg-gray-200'>
+        <div className="text-center py-8 || content custom-maxWidth">
+          <h1 className='text-2xl mb-2 lg:text-5xl text-center'>
+            Drive your crypto investments to success<br/> with our  real-time tracking app.
+          </h1>
+          <p className='mb-6'>
+            Track crypto prices in real-time with our user-friendly app. 
+            Get market insights, set alerts, and stay informed on latest developments.<br/>  
+            Take control of your investments and achieve financial goals.
+          </p>
+          <div className='search'>
+          <span className='block text-gray-700 font-bold mb-2'>Search crypto: </span>
+            <input className='bg-gray-100 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 max-w-lg' type="text" value={store.query} onChange={store.setQuery} />
+          </div>
+        </div>
+      </div>
       {store.coins.map(coin => {
         return (
           <div key={coin.id}>
